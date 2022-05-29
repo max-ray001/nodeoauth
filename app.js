6a69a192f80b03b4b3796dd8da5ce46e6ec5927c
onosaito <onosaito0614@gmail.com>
2022-05-29 18:32:45 +0000

    removed the method override state

--- app.js
+++ app.js
@@ -55,16 +55,16 @@ if (cluster.isMaster) {
   });
 
   //method override
-  app.use(
-    methodOverride(function (req, res) {
-      if (req.body && typeof req.body === "object" && "_method" in req.body) {
-        // look in urlencoded POST bodies and delete it
-        let method = req.body._method;
-        delete req.body._method;
-        return method;
-      }
-    })
-  );
+//   app.use(
+//     methodOverride(function (req, res) {
+//       if (req.body && typeof req.body === "object" && "_method" in req.body) {
+//         // look in urlencoded POST bodies and delete it
+//         let method = req.body._method;
+//         delete req.body._method;
+//         return method;
+//       }
+//     })
+//   );
 
   //register handlebars helpers
   const {
